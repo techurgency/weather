4181ffb7551462cd08a05621ef73d5c28ec9e653
techurgency <68974295+techurgency@users.noreply.github.com>
2020-10-23 15:41:29 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+
+  res.sendFile(__dirname + "/index.html");
+
+
+
+});
+
+app.post("/", function(req,res){
+  const query = req.body.cityName
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=2ccb6b87aa7134c4c70af62165527fc5&units=imperial#";
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const iconURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<h1>The temp is " + temp + " degrees.</h1>");
+      res.write("<h2>There are " + weatherDescription + "!");
+      res.write("<h3><img src=" + iconURL + "></h3>");
+      res.send();
+    })
+  })
+})
+
+app.listen(3000, function(){
+  console.log("3000");
+})
